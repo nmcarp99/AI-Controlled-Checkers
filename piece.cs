e4851be5765e31511ccb5cd004705cf9a5692557
nmcarp99 <nmcarp99@gmail.com>
2020-12-10 14:01:27 -0500

    can move checkers, can overlap

--- piece.cs
+++ piece.cs
@@ -3,7 +3,16 @@ using System;
 class piece {
 	public int[] position = new int[] {0, 0};
 	public string value = "";
-
+	public void move(int[] relativePosition) {
+		if (position[0] + relativePosition[0] < 8 && 
+		position[0] + relativePosition[0] > -1) {
+			if (position[1] + relativePosition[1] < 8 && 
+			position[1] + relativePosition[1] > -1) {
+				position[0] += relativePosition[0];
+				position[1] += relativePosition[1];
+			}
+		}
+	}
 	public piece(string newValue, int[] newPosition) {
 		position = newPosition;
 		value = newValue;
